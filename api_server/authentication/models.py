774ac41707389bf80e1660a0eff409b6376b8baf
Bharath6730 <thanigaivelbharath@gmail.com>
2023-12-13 07:34:55 +0530

    Fix db migrate issue

--- api_server/authentication/models.py
+++ api_server/authentication/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True, primary_key=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self): 
+        return self.name
+    
+
+def get_default_admin_role():
+    return Role.objects.get_or_create(name='admin',description="Admin Role")[0]
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email: raise ValueError('The Email field must be set.')
@@ -15,17 +26,11 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_student', False)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('role', Role.objects.get(name='admin') )
+        super_admin_role= Role.objects.get_or_create(name='super_admin',description="Super Admin Role")[0]
+        extra_fields.setdefault('role', super_admin_role )
         return self.create_user(email, password, **extra_fields)
     
 
-class Role(models.Model):
-    name = models.CharField(max_length=50, unique=True, primary_key=True)
-    description = models.TextField(null=True, blank=True)
-
-    def __str__(self): 
-        return self.name
-     
 class User(AbstractBaseUser, PermissionsMixin):
 
     last_login = None
@@ -36,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=30,null=False)
     last_name = models.CharField(max_length=30,null=False)
     reg_or_emp_id  = models.CharField(unique=True,null=False)
-    role = models.ForeignKey(Role, on_delete=models.PROTECT, default= Role.objects.get(name='student',))
+    role = models.ForeignKey(Role, on_delete=models.PROTECT,null=False)
     is_student = models.BooleanField(default=True)
     
     updated_at = models.DateTimeField(auto_now=True)
@@ -45,7 +50,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email' 
-    REQUIRED_FIELDS = ['first_name', 'last_name','reg_or_emp_id',] 
+    REQUIRED_FIELDS = ['first_name', 'last_name','reg_or_emp_id'] 
 
     def __str__(self): 
         return self.reg_or_emp_id
+    
+    def save(self, *args, **kwargs):
+        if self.role is None: 
+            self.role = Role.objects.get_or_create(name='student',description="Student Role")[0]
+        super(User, self).save(*args, **kwargs)
--- api_server/authentication/serializers.py
+++ api_server/authentication/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from .models import User
+from .models import Role, User
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, write_only=True)
+    role = serializers.CharField(required=False, allow_blank=True,trim_whitespace=True)
 
-    def create(self, validated_data):
+    def create(self, validated_data : dict):
         password = validated_data.pop("password", None)
+        role = validated_data.pop("role", None)
         instance = self.Meta.model(**validated_data)
+        if role is not None:
+            instance.role = Role.objects.get_or_create(name = role)[0]
         if password is not None:
             instance.set_password(password)
         instance.save()
@@ -15,7 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = [ "reg_or_emp_id", "email", "password", "first_name", "last_name","role" ]
+        fields = [ "reg_or_emp_id", "email", "password", "first_name", "last_name","role"]
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
